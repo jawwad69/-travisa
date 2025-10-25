483ccc456ad87bc767567a2b0379e7d770443269
jawwad69 <ashahemeoki@gmail.com>
2025-10-25 16:28:37 +0530

    Add files via upload

---
+++ final-porject - Copy (end)/asseds/java-script/script.js
@@ -0,0 +1,41 @@
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+// this is counter
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+
+    counter("count1", 0, 49, 3000); 
+    counter("count4", 0, 98, 3000); 
+    counter("count2", 100, 377, 2500); 
+    counter("count3", 0, 31, 3000);
+   });
